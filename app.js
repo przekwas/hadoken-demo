5f28380bf5544f1091c0ed1e5966dc482f524740
Luke Przkwas <luke@covalence.io>
2023-04-07 21:23:46 -0500

    style updates

--- app.js
+++ app.js
@@ -1,18 +1,65 @@
-const STANDING = 'standing.gif';
-const HADOKEN = 'hadoken.gif';
+const imgSrcs = {
+	standing: 'standing.gif',
+	hadoken: 'hadoken.gif'
+};
 
-const RYU = document.createElement('img');
-RYU.src = STANDING;
+const elements = {
+	ryuDiv: document.querySelector('#img'),
+	buttonX: document.querySelector('#x'),
+	buttonDown: document.querySelector('#down'),
+	buttonRight: document.querySelector('#right'),
+	ryuImg: document.querySelector('img'),
+	animateRyu: function () {
+		this.ryuImg.src = imgSrcs.hadoken;
+		setTimeout(() => (this.ryuImg.src = imgSrcs.standing), 980);
+	},
+	buttonPress: function (buttonType) {
+		this[buttonType].style.background = '#A9A9A9';
+	},
+	buttonRelease: function (buttonType) {
+		this[buttonType].style.background = '#FFFFFF';
+	}
+};
+
+elements.ryuImg.src = imgSrcs.standing;
 
 const listener = new window.keypress.Listener();
-document.body.appendChild(RYU);
+
+listener.register_many([
+	{
+		keys: 'x',
+		on_keydown: function () {
+			elements.buttonPress('buttonX');
+		},
+		on_keyup: function () {
+			elements.buttonRelease('buttonX');
+		}
+	},
+	{
+		keys: 'down',
+		on_keydown: function () {
+			elements.buttonPress('buttonDown');
+		},
+		on_keyup: function () {
+			elements.buttonRelease('buttonDown');
+		}
+	},
+	{
+		keys: 'right',
+		on_keydown: function () {
+			elements.buttonPress('buttonRight');
+		},
+		on_keyup: function () {
+			elements.buttonRelease('buttonRight');
+		}
+	}
+]);
 
 listener.sequence_combo(
 	'down right x',
-	function (evy) {
+	function () {
 		console.log('HADOKEN!');
-		RYU.src = HADOKEN;
-		setTimeout(() => (RYU.src = STANDING), 980);
+		elements.animateRyu();
 	},
 	true
 );
